f584fcd7faed621019ff298534881aefbf72fcb6
Mahen <mahen.ath@outlook.com>
2023-01-30 15:36:00 +0100

    8 logo animation

--- js/textAnim.js
+++ js/textAnim.js
@@ -1,8 +1,12 @@
-const circles = document.getElementById("circles")
+document.addEventListener('mousemove', parralax);
 
-document.addEventListener('scroll', (e)=>{
-    const scrollY = window.scrollY
-    if(scrollY <= 0){
-        circles.style.gap = "30px";
-    }
-})
+function parralax(e){
+  document.querySelectorAll(".movement").forEach(function(move) {
+    const moving_value = move.getAttribute("data-value");
+    let x = e.clientX * moving_value / 250;
+    let y = e.clientY * moving_value / 250;
+
+    move.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+
+}
